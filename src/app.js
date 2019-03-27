9ce2c73572f9622679280d08f286df4175254c2e
NexusDesignsCo <nexusdesigns.co@gmail.com>
2019-03-26 17:02:03 -0700

    init commit

---
+++ src/app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const hbs = require("hbs");
+const path = require("path");
+
+app.use(express.static('public'));
+
+app.set('view engine', 'hbs');
+app.set("views", path.join(__dirname, "../templates/views"));
+hbs.registerPartials(path.join(__dirname, "../templates/partials"));
+
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.render("index.hbs");
+});
+
+app.listen(port, () => {
+  console.log("listening on port: " + port);
+})
