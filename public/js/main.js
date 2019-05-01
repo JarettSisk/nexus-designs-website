6f77663299727a278cd23608153e55db9670674b
NexusDesignsCo <nexusdesigns.co@gmail.com>
2019-05-01 09:46:49 -0700

    added load screen

--- public/js/main.js
+++ public/js/main.js
@@ -5,3 +5,9 @@ let navLinks = document.querySelector(".navLinks");
 function hide() {
   navLinks.classList.toggle("show");
 }
+
+//for the loading screen
+window.addEventListener("load", function () {
+  const loader = document.querySelector(".loaderDiv");
+  loader.classList.add("hidden");
+})
